906e4b44c8d4cfc04b6ed7612db8c7ad6ef6c552
spiritlhl <103393591+spiritLHLS@users.noreply.github.com>
2023-09-28 15:04:52 +0800

    Update rsync_eggs.sh

--- eggs/rsync_eggs.sh
+++ eggs/rsync_eggs.sh
@@ -6,9 +6,21 @@
 
 cd /root >/dev/null 2>&1
 if [ -f "/var/www/pterodactyl/database/Seeders/eggs" ]; then
-    curl -slk https://github.com/spiritLHLS/pterodactyl/archive/refs/heads/main.zip -o pterodactyl_eggs.zip
-    chmod 777 pterodactyl_eggs.zip
-    unzip pterodactyl_eggs.zip
-    cp -r ./pterodactyl_eggs/pterodactyl-main/eggs /var/www/pterodactyl/database/Seeders/eggs
-    rm -rf pterodactyl_eggs*
+    wget https://github.com/spiritLHLS/pterodactyl/archive/refs/heads/main.zip
+    chmod 777 main.zip
+    unzip main.zip
+    if [ ! -f "/var/www/pterodactyl/database/Seeders/eggs/vps" ]; then
+        mkdir -p /var/www/pterodactyl/database/Seeders/eggs/vps
+    fi
+    if [ ! -f "/var/www/pterodactyl/database/Seeders/eggs/vps/debian" ]; then
+        mkdir -p /var/www/pterodactyl/database/Seeders/eggs/vps/debian
+    fi
+    if [ ! -f "/var/www/pterodactyl/database/Seeders/eggs/vps/ubuntu" ]; then
+        mkdir -p /var/www/pterodactyl/database/Seeders/eggs/vps/ubuntu
+    fi
+    cp ./pterodactyl-main/eggs/debian/* /var/www/pterodactyl/database/Seeders/eggs/vps/debian/
+    cp ./pterodactyl-main/eggs/ubuntu/* /var/www/pterodactyl/database/Seeders/eggs/vps/ubuntu/
+    rm -rf pterodactyl_eggs* 
+    rm -rf pterodactyl-main*
+    rm -rf main.zip
 fi
